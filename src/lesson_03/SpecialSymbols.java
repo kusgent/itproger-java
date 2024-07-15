90f0e09c489b22bca5bbe41e0b781264866c5306
Kustov Ilya <kusgent@yandex.ru>
2024-07-15 12:12:44 +0300

    Add special symbols

---
+++ src/lesson_03/SpecialSymbols.java
@@ -0,0 +1,20 @@
+package lesson_03;
+
+public class SpecialSymbols {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello\nWorld!");
+        System.out.println("Hello\n\n\nWorld!");
+        System.out.println("Hello\n\n\tWorld!");
+        System.out.println("Hello\"World!");
+        System.out.println("Hello\\World!");
+
+        // comment
+        /*
+        comment
+         */
+
+    }
+
+}
