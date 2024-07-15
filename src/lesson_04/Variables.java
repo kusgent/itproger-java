bedbb4b85d2257a81bc3d0e423c06628f8a2310e
Kustov Ilya <kusgent@yandex.ru>
2024-07-15 12:32:40 +0300

    Add variables

---
+++ src/lesson_04/Variables.java
@@ -0,0 +1,18 @@
+package lesson_04;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        System.out.println("Переменные");
+
+        int age = 34;
+        age = 35;
+        System.out.println("Возраст: " + age + ".");
+
+        int num = -10678;
+        System.out.println(num + " - отрицательное число");
+
+    }
+
+}
