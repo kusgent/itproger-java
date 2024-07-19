fe30005e176cffc7cff5720cf67367a1564b351a
Kustov Ilya <kusgent@yandex.ru>
2024-07-19 14:24:51 +0300

    Add data types

---
+++ src/lesson_04/DataTypes.java
@@ -0,0 +1,23 @@
+package lesson_04;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        System.out.println("Типы данных" + "\n");
+        byte num1 = 32;
+        System.out.println(num1);
+        short num2 = 32000;
+        System.out.println(num2);
+        int num3 = 320000000;
+        System.out.println(num3);
+        long num4 = 3200000000000000000L;
+        System.out.println(num4);
+        float num5 = 3.14f;
+        System.out.println(num5);
+        double num6 = 3.14;
+        System.out.println(num6);
+        char ch = '7';
+        System.out.println(ch);
+        boolean isHappy = true;
+        System.out.println(isHappy);
+    }
+}
