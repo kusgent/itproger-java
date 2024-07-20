e5f8c376f0977f68f745b8ea9478a50342b6225e
Kustov Ilya <kusgent@yandex.ru>
2024-07-20 18:50:25 +0300

    Add mathematical operations

---
+++ src/lesson_05/MathOperations.java
@@ -0,0 +1,19 @@
+package lesson_05;
+
+import java.util.Scanner;
+
+public class MathOperations {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите 1е число: ");
+        float num1 = scan.nextFloat();
+        System.out.println("Введите 2е число: ");
+        float num2 = scan.nextFloat();
+        float res1 = num1 + num2;
+        float res2 = num1 - num2;
+        float res3 = num1 * num2;
+        float res4 = num1 / num2;
+        System.out.println("Результат: ");
+        System.out.println(res1 + "\n" + res2 + "\n" + res3 + "\n" + res4);
+    }
+}
