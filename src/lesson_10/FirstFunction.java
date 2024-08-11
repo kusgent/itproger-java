1b528806e939d0a4949b76afdafeac0ffd7a30bf
Kustov Ilya <kusgent@yandex.ru>
2024-08-11 16:15:19 +0300

    Add first function

---
+++ src/lesson_10/FirstFunction.java
@@ -0,0 +1,12 @@
+package lesson_10;
+
+public class FirstFunction {
+    public static void main(String[] args) {
+        info();
+        info();
+        info();
+    }
+    public static void info() {
+        System.out.println("Hello");
+    }
+}
